2354fb111122900af67fc96d10e012c844c59c5c
Xursian <derek.lj.ranger@gmail.com>
2013-01-29 15:39:26 -0600

    added and changed into files

--- models/customer.rb
+++ models/customer.rb
@@ -4,9 +4,9 @@ class Customer < ActiveRecord::Base
   # Customer have a many to one relationship with a province by
   # way of a province_id foreign key. In other words, a customer
   # belongs to a specific province.
-  belongs_to :province
+#  belongs_to :province
   
   # If any of these three columns are blank then we cannot persist
   # the customer to the database.
-  validates_presence_of :name, :email, :city
+#  validates_presence_of :name, :email, :city
 end
---
+++ practice_read.rb
@@ -0,0 +1,4 @@
+load 'ar.rb'
+require 'pp' #pretty print library in ruby
+
+pp Customer.first.names
